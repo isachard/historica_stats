f5639af49c59dd599a1143b2094a666d32062205
isachard <isachard.rodriguez@pcc.edu>
2020-03-22 16:03:21 -0700

    formatted dataset for better inclusion

---
+++ mlb/mlbtrends.py
@@ -0,0 +1,6 @@
+import pandas as pd
+df = pd.read_csv('data/arizona.txt', sep="   ", header=None)
+
+
+#file = open("data/arizona.txt")
+#print(file.readlines())
---
+++ untitled5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 22 14:19:02 2020
+
+@author: shka
+"""
+
+import pandas as pd
+df = pd.read_csv("mlb/data/test.txt")
+df.columns = ['Date', 'Team', 'Result', 'Score', 'PitcherF','PitcherL', 
+              'Pitcher H', 'Opponent PitcherF',' Opponent PitcherL', 
+              'Opponent Pitcher H', 'Result Run Line','Run Line Value',
+              'Result Total', 'Total Line', 'Total Value']
+  
+
+
+print(df.to_string())
